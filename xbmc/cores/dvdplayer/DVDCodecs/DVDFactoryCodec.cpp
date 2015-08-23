154a3eb05776b84ee6d79d4ca39057165da6ba57
Owersun <owersun@gmail.com>
2015-08-23 15:46:14 +0200

    C1: Own codec implementation

--- xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/DVDFactoryCodec.cpp
@@ -53,6 +53,9 @@
 #if defined(HAS_MFC)
 #include "Video/DVDVideoCodecMFC.h"
 #endif
+#if defined(HAS_C1)
+#include "Video/DVDVideoCodecC1.h"
+#endif
 #include "Audio/DVDAudioCodecFFmpeg.h"
 #include "Audio/DVDAudioCodecPassthrough.h"
 #include "Overlay/DVDOverlayCodecSSA.h"
@@ -207,6 +210,11 @@ CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec(CDVDStreamInfo &hint, const C
 #elif defined(_LINUX)
   hwSupport += "MFC:no ";
 #endif
+#if defined(HAS_C1) && defined(_LINUX)
+  hwSupport += "C1:yes ";
+#elif defined(_LINUX)
+  hwSupport += "C1:no ";
+#endif
   CLog::Log(LOGDEBUG, "CDVDFactoryCodec: compiled in hardware support: %s", hwSupport.c_str());
 
   if (hint.stills && (hint.codec == AV_CODEC_ID_MPEG2VIDEO || hint.codec == AV_CODEC_ID_MPEG1VIDEO))
@@ -214,6 +222,22 @@ CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec(CDVDStreamInfo &hint, const C
      // If dvd is an mpeg2 and hint.stills
      if ( (pCodec = OpenCodec(new CDVDVideoCodecLibMpeg2(), hint, options)) ) return pCodec;
   }
+
+#if defined(HAS_MFC)
+  if ( !hint.software )
+  {
+    if ( hint.codec == AV_CODEC_ID_H263 || hint.codec == AV_CODEC_ID_H264 || hint.codec == AV_CODEC_ID_MPEG4 || hint.codec == AV_CODEC_ID_MPEG2VIDEO || AV_CODEC_ID_MPEG1VIDEO || hint.codec == AV_CODEC_ID_VC1 )
+      if( (pCodec = OpenCodec(new CDVDVideoCodecMFC(), hint, options)) ) return pCodec;
+  }
+#endif
+
+#if defined(HAS_C1)
+  if ( !hint.software )
+  {
+    if ( hint.codec == AV_CODEC_ID_H263 || hint.codec == AV_CODEC_ID_MPEG4 || hint.codec == AV_CODEC_ID_H264 || hint.codec == AV_CODEC_ID_HEVC )
+      if( (pCodec = OpenCodec(new CDVDVideoCodecC1(), hint, options)) ) return pCodec;
+  }
+#endif
 
 #if defined(HAS_LIBAMCODEC)
   // amcodec can handle dvd playback.
@@ -327,14 +351,6 @@ CDVDVideoCodec* CDVDFactoryCodec::CreateVideoCodec(CDVDStreamInfo &hint, const C
         default:
           if ( (pCodec = OpenCodec(new CDVDVideoCodecStageFright(), hint, options)) ) return pCodec;
       }
-    }
-#endif
-
-#if defined(HAS_MFC)
-    if ( !hint.software )
-    {
-      if ( hint.codec == AV_CODEC_ID_H263 || hint.codec == AV_CODEC_ID_H264 || hint.codec == AV_CODEC_ID_MPEG4 || hint.codec == AV_CODEC_ID_MPEG2VIDEO || AV_CODEC_ID_MPEG1VIDEO || hint.codec == AV_CODEC_ID_VC1 )
-        if( (pCodec = OpenCodec(new CDVDVideoCodecMFC(), hint, options)) ) return pCodec;
     }
 #endif
 
